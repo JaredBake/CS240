63f85068520e28d3c1d22b8ee7ef2d4e525c792d
JaredBake <18jabake@gmail.com>
2023-10-30 16:28:47 -0600

    updated class comments

--- MyChess/src/main/Server/Requests/LoginRequest.java
+++ MyChess/src/main/Server/Requests/LoginRequest.java
@@ -1,8 +1,20 @@
 package Server.Requests;
 
+/**
+ * class to attempt logging in
+ */
 public class LoginRequest {
+    /**
+     * unique username for the account
+     */
     private String username;
+    /**
+     * password for the unique username
+     */
     private String password;
+    /**
+     * the request is being sent in using the username and password with this function
+     */
     public LoginRequest() {}
-    // … Getters and Setters for username and password properties
+    // Getters and Setters for username and password properties
 }
--- MyChess/src/main/Server/Results/LoginResult.java
+++ MyChess/src/main/Server/Results/LoginResult.java
@@ -1,13 +1,31 @@
 package Server.Results;
 
+/**
+ * Class for returning the result of the attempted login
+ */
 public class LoginResult {
+    /**
+     * the message returned about the result
+     */
     private String message;
+    /**
+     * the token given to verify login access
+     */
     private String authToken;
+    /**
+     * unique username returned with result
+     */
     private String username;
+    /**
+     * A function that returns the result of the login attempt
+     */
     public LoginResult() {}
 
     /**
      * Getters and Setters for message, authToken, and username properties
      */
+    public String getToken(){
+        return null;
+    }
 
 }
