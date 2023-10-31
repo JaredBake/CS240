dc0c6d81908e2f79faf162b79cc2e55d0d7f18c8
JaredBake <18jabake@gmail.com>
2023-10-30 20:07:47 -0600

    Added javadocs

--- MyChess/src/javadocs/package-search-index.js
+++ MyChess/src/javadocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"chess"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"chess"},{"l":"dataAccess"},{"l":"Server.DataAccess"},{"l":"Server.Model"},{"l":"Server.Requests"},{"l":"Server.Results"},{"l":"Server.services"}];updateSearchResults();
